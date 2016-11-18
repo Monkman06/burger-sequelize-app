7ebe4262e061ae4b20463ecd79ffc802fdcae59d
Monkman06 <ptkcpt2012@gmail.com>
2016-11-18 16:41:50 -0500

    mods added

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Knicks00',
+    password: '',
     database: 'burgers_db'
 });
 
